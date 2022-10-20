41ba9d5efd31a0f378379e0e4717db771ea045b7
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-20 11:54:09 +0530

    conditional rendering

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,28 +5,29 @@ export default function App() {
 
 const name = "Adarsh";
 
-
+const isLoggedin = false;
 
 const returnHTML = (
-  <>
+  
                   <div className="App">
                   <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
                     <p>
-                      <h1>Hello world</h1>
+                      
                       {/* since this is a jsx file that is combibatin on html and js this whole code change into pure j susing Babel
                       and in order to use js we have to use curly bracket and note we can only write one line of code */}
-                      {console.log("hello world")}
-                      {name}
+                      {/* <h1>Hello{isLoggedin?name:' World'} </h1> */}
+                      {!isLoggedin && <p>Hello World</p> }
+                      {isLoggedin && <p>Hello {name} </p> }
+                      {/* now in jsx if we have (true && statement) it will return statement */}
+                      
                     </p>
                     
                   </header>
                   </div>
-                  <span>
-                    Adarsh
-                  </span>
                   
-                  </>
+                  
+                  
 );
 // please note that we cannot return two tags indepenedtly for that we must have to wrap them into a single tag or we can use react fragments
                   // as done
