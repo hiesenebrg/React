b5211bc5ffba86de858a3dd6245124bae40bc679
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-20 11:30:03 +0530

    App.js study

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>Hello world</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+const name = "Adarsh";
+
+
+
+const returnHTML = (
+                  <div className="App">
+                  <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                      <h1>Hello world</h1>
+                      {/* since this is a jsx file that is combibatin on html and js this whole code change into pure j susing Babel
+                      and in order to use js we have to use curly bracket and note we can only write one line of code */}
+                      {console.log("hello world")}
+                      {name}
+                    </p>
+                    
+                  </header>
+                  </div>
+);
+  
+
+  return returnHTML;
+    
+  
 }
 
-export default App;
+
