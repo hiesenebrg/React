5192e4b95787c9cf39a6d4643a2f4e3dac89e49a
hiesenebrg <adarshsingh8008@gmail.com>
2022-10-28 08:28:30 +0530

    other changes made

--- cart/src/index.js
+++ cart/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Cart from './Cart';
+import Navbar from './Navbar';
+import CartItem from './CartItem';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
